2707ca6db7128dec56a2db34e39978b257103122
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2018-03-01 15:08:02 +0000

    DRY up code

--- tasklist/app.js
+++ tasklist/app.js
@@ -3,45 +3,43 @@ const taskInput = document.querySelector('#task');
 const filterInput = document.querySelector('#filter')
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.card-action .btn')
+let tasks;
 
-const getLocal = () => {
-  let tasks;
+const checkStorage = (x) => {
   if(localStorage.getItem('tasks') === null) {
     tasks = [];
   } else {
     tasks = JSON.parse(localStorage.getItem('tasks'));
-  }
+  };
+};
+
+const appendToList = (li, text, link) => {
+  li.className = 'collection-item';
+  link.className = 'delete-item secondary-content';
+  link.innerHTML = '<i class="fa fa-trash"></i>';
+  li.appendChild(text);
+  li.appendChild(link);
+  taskList.appendChild(li);
+}
+
+const getLocal = () => {
+  checkStorage(tasks);
   for (let task of tasks) {
     const li = document.createElement('li');
     const text = document.createTextNode(task);
     const link = document.createElement('a');
-    li.className = 'collection-item';
-    link.className = 'delete-item secondary-content';
-    link.innerHTML = '<i class="fa fa-trash"></i>';
-    li.appendChild(text);
-    li.appendChild(link);
-    taskList.appendChild(li);
+    appendToList(li, text, link);
   };
 };
 
 const storeLocal = (task) => {
-  let tasks;
-  if(localStorage.getItem('tasks') === null) {
-    tasks = [];
-  } else {
-    tasks = JSON.parse(localStorage.getItem('tasks'));
-  }
+  checkStorage(tasks);
   tasks.push(task);
   localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 const removeFromLocal = (taskItem) => {
-  let tasks;
-  if(localStorage.getItem('tasks') === null) {
-    tasks = [];
-  } else {
-    tasks = JSON.parse(localStorage.getItem('tasks'));
-  }
+  checkStorage(tasks);
   tasks.forEach((task, index) => {
     if(taskItem.textContent === task) {
       tasks.splice(index, 1);
@@ -51,18 +49,13 @@ const removeFromLocal = (taskItem) => {
 }
 
 const addTask = (e) => {
+  e.preventDefault();
   const li = document.createElement('li');
   const text = document.createTextNode(taskInput.value);
   const link = document.createElement('a');
-  li.className = 'collection-item';
-  link.className = 'delete-item secondary-content';
-  link.innerHTML = '<i class="fa fa-trash"></i>';
-  li.appendChild(text);
-  li.appendChild(link);
-  taskList.appendChild(li);
+  appendToList(li, text, link);
   storeLocal(taskInput.value);
   taskInput.value = "";
-  e.preventDefault();
 };
 
 const deleteTask = (e) => {
